6b1e5cdc6ca05be1d129c13bab5d17448aa44b3c
sogawa-yk <daybreaks.link@gmail.com>
2023-12-26 17:06:28 +0900

    a

--- basic_env/provider.tf
+++ basic_env/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "5.23.0"
+    }
+  }
+}
+
 provider "oci" {
   tenancy_ocid = var.tenancy_ocid
   region       = var.region
