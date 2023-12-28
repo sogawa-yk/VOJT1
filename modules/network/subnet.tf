ec1219d790912aa8a7ef94857740754c892f53e5
sogawa-yk <daybreaks.link@gmail.com>
2023-12-28 14:27:02 +0900

    a

--- modules/network/subnet.tf
+++ modules/network/subnet.tf
@@ -9,6 +9,6 @@ resource "oci_core_subnet" "subnet" {
   # Caution: For the route table id, use module.vcn.nat_route_id.
   # Do not use module.vcn.nat_gateway_id, because it is the OCID for the gateway and not the route table.
   route_table_id    = oci_core_route_table.route_table.id
-  security_list_ids = [oci_core_security_list.private-security-list.id]
+  security_list_ids = [oci_core_security_list.security-list.id]
   display_name      = var.subnet_display_name
 }
