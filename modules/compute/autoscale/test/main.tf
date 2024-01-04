bffbae5a7291f777fde30feaef9dad17608daef5
sogawa-yk <daybreaks.link@gmail.com>
2024-01-04 18:02:02 +0900

    fix

--- modules/compute/autoscale/test/main.tf
+++ modules/compute/autoscale/test/main.tf
@@ -3,7 +3,6 @@ resource "oci_core_instance_configuration" "test_instance_configuration" {
   compartment_id = var.compartment_ocid
 
   #Optional
-  defined_tags  = { "Operations.CostCenter" = "42" }
   display_name  = var.instance_configuration_display_name
   freeform_tags = { "Department" = "Finance" }
   instance_details {
@@ -20,10 +19,8 @@ resource "oci_core_instance_configuration" "test_instance_configuration" {
 
         #Optional
         assign_public_ip = true
-        defined_tags     = { "Operations.CostCenter" = "42" }
         subnet_id        = module.vcn.subnet_id
       }
-      defined_tags = { "Operations.CostCenter" = "42" }
       # display_name = var.display_name
       # fault_domain = var.fault_domain # 指定しない場合はシステムが自動で設定する
       launch_mode = "NATIVE"
