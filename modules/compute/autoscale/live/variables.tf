7b910d634ca070dc6afdb5dd2fad782d8eba5842
sogawa-yk <daybreaks.link@gmail.com>
2024-01-04 20:33:21 +0900

    fix

--- modules/compute/autoscale/live/variables.tf
+++ modules/compute/autoscale/live/variables.tf
@@ -1,9 +1,18 @@
+### COMMON ##############################################
 variable "compartment_id" {}
-variable "instance_configuration_display_name" {}
 variable "availability_domain" {}
+variable "subnet_id" {}
+
+### INSTANCE_CONFIGURATION ##############################
 variable "metadata" {}
 variable "shape" {
   type    = string
   default = "VM.Standard2.1"
 }
-variable "subnet_id" {}
+variable "instance_configuration_display_name" {}
+
+### INSTANCE_POOL #######################################
+variable "instance_pool_display_name" {}
+variable "instance_pool_display_name_formatter" {}
+variable "instance_pool_size" {}
+
