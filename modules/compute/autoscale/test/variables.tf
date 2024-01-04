7590e6d843df93cf07c5ae7f4e3293a9145f036f
sogawa-yk <daybreaks.link@gmail.com>
2024-01-04 17:58:58 +0900

    fix

--- modules/compute/autoscale/test/variables.tf
+++ modules/compute/autoscale/test/variables.tf
@@ -3,7 +3,6 @@ variable "instance_configuration_display_name" {
   type    = string
   default = "test"
 }
-variable "metadata" {}
 variable "shape" {
   type    = string
   default = "VM.Standard2.1"
