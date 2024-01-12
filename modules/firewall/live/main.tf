63972510d4a2e20b4f99bc001e7f5a9c288815f9
sogawa-yk <daybreaks.link@gmail.com>
2024-01-12 12:34:34 +0900

    add firewall

---
+++ modules/firewall/live/main.tf
@@ -0,0 +1,51 @@
+resource "oci_network_firewall_network_firewall" "test_network_firewall" {
+  #Required
+  compartment_id             = var.compartment_id
+  network_firewall_policy_id = oci_network_firewall_network_firewall_policy.test_network_firewall_policy.id
+  subnet_id                  = var.subnet_id
+
+  #Optional
+  display_name = "test-network-firewall"
+}
+
+resource "oci_network_firewall_network_firewall_policy" "test_network_firewall_policy" {
+  #Required
+  compartment_id = var.compartment_id
+
+  #Optional
+  display_name = var.network_firewall_policy_display_name
+}
+
+resource "oci_network_firewall_network_firewall_policy_service" "http_network_firewall_policy_service" {
+  #Required
+  name                       = "http_policy"
+  network_firewall_policy_id = oci_network_firewall_network_firewall_policy.test_network_firewall_policy.id
+  port_ranges {
+    #Required
+    minimum_port = 80
+
+    #Optional
+    maximum_port = 80
+  }
+}
+
+resource "oci_network_firewall_network_firewall_policy_service" "ssh_network_firewall_policy_service" {
+  #Required
+  name                       = "ssh_policy"
+  network_firewall_policy_id = oci_network_firewall_network_firewall_policy.test_network_firewall_policy.id
+  port_ranges {
+    #Required
+    minimum_port = 22
+
+    #Optional
+    maximum_port = 22
+  }
+}
+
+resource "oci_network_firewall_network_firewall_policy_service_list" "test_network_firewall_policy_service_list" {
+  #Required
+  name                       = var.network_firewall_policy_service_list_name
+  network_firewall_policy_id = oci_network_firewall_network_firewall_policy.test_network_firewall_policy.id
+  services = [oci_network_firewall_network_firewall_policy_service.http_network_firewall_policy_service.name,
+  oci_network_firewall_network_firewall_policy_service.ssh_network_firewall_policy_service.name]
+}
