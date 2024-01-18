db24522be8c4b5845d464fd9b3f73bba2ee0416f
sogawa-yk <daybreaks.link@gmail.com>
2024-01-18 16:15:07 +0900

    add instance

--- modules/monitoring/test/main.tf
+++ modules/monitoring/test/main.tf
@@ -18,19 +18,32 @@ module "test_monitoring" {
   alarm_suppression_time_suppress_from                = var.alarm_suppression_time_suppress_from
   alarm_suppression_time_suppress_until               = var.alarm_suppression_time_suppress_until
   alarm_suppression_description                       = var.alarm_suppression_description
-  notification_topic_name = var.notification_topic_name
+  notification_topic_name                             = var.notification_topic_name
   notification_topic_description                      = var.notification_topic_description
-  subscription_endpoint = var.subscription_endpoint
-  subscription_protocol = var.subscription_protocol
+  subscription_endpoint                               = var.subscription_endpoint
+  subscription_protocol                               = var.subscription_protocol
 }
 
-module "test_instance" {
+module "test_instance1" {
   source              = "../../compute/compute/live"
   compartment_id      = var.compartment_ocid
   shape               = var.shape
   source_id           = var.source_id
   source_type         = var.source_type
-  display_name        = var.display_name
+  display_name        = var.display_name_1
+  subnet_id           = module.sample_network.public_subnet_id
+  oci_ad_name         = data.oci_identity_availability_domains.ads.availability_domains[0].name
+  ssh_authorized_keys = base64decode(data.oci_secrets_secretbundle.ssh_public_key.secret_bundle_content.0.content)
+  server_port         = var.server_port
+}
+
+module "test_instance2" {
+  source              = "../../compute/compute/live"
+  compartment_id      = var.compartment_ocid
+  shape               = var.shape
+  source_id           = var.source_id
+  source_type         = var.source_type
+  display_name        = var.display_name_2
   subnet_id           = module.sample_network.public_subnet_id
   oci_ad_name         = data.oci_identity_availability_domains.ads.availability_domains[0].name
   ssh_authorized_keys = base64decode(data.oci_secrets_secretbundle.ssh_public_key.secret_bundle_content.0.content)
