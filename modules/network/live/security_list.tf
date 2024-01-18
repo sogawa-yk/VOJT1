7de3bec69fd252063b627f3fd444902de23216eb
sogawa-yk <daybreaks.link@gmail.com>
2024-01-18 10:28:40 +0900

    add sl

--- modules/network/live/security_list.tf
+++ modules/network/live/security_list.tf
@@ -30,6 +30,17 @@ resource "oci_core_security_list" "public_security_list" {
     stateless   = false
     source      = "0.0.0.0/0"
     source_type = "CIDR_BLOCK"
+    # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml TCP is 6
+    protocol = "6"
+    tcp_options {
+      min = 22
+      max = 22
+    }
+  }
+  ingress_security_rules {
+    stateless   = false
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
     # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml ICMP is 1  
     protocol = "1"
 
