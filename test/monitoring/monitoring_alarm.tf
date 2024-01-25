ba114fcaa8497959c044a07f594dda8d917294f0
sogawa-yk <daybreaks.link@gmail.com>
2024-01-25 16:36:03 +0900

    fix

--- test/monitoring/monitoring_alarm.tf
+++ test/monitoring/monitoring_alarm.tf
@@ -7,8 +7,22 @@ module "monitoring_alarm" {
 locals {
   updated_monitoring_alarms = {
     for key, alarm in var.monitoring_alarms : key => {
-      destinations          = [module.notification_topic.topic_map["topic001"]]
-      metric_compartment_id = var.compartment_ocid
+      destinations                                  = [module.notification_topic.topic_map["topic001"]]
+      metric_compartment_id                         = var.compartment_ocid
+      display_name                                  = alarm.display_name
+      is_enabled                                    = alarm.is_enabled
+      namespace                                     = alarm.is_namespace
+      query                                         = alarm.query
+      severity                                      = alarm.severity
+      body                                          = alarm.body
+      is_notifications_per_metric_dimension_enabled = alarm.is_notifications_per_metric_dimension_enabled
+      message_format                                = alarm.message_format
+      metric_compartment_id_in_subtree              = alarm.metric_compratment_id_in_subtree
+      pending_duration                              = alarm.pending_duration
+      repeat_notification_duration                  = alarm.repeat_notification_duration
+      resolution                                    = alarm.resolution
+      resouce_group                                 = alarm.resource_group
+      suppression                                   = alarm.suppression
     }
   }
 }
